fd08e4fa3405ad04ec7f279da63ff7717a9e54c6
sumit2k7 <sumit2k7@users.noreply.github.com>
2016-04-29 12:18:42 +0530

    Added files via upload

---
+++ Session7Assignment3.java
@@ -0,0 +1,14 @@
+
+public class Session7Assignment3 {
+
+	public static void main(String[] args) {
+try
+{
+		        String s = null;
+		        System.out.println(s.toString()); 
+		    }
+catch(NullPointerException nue)
+{ System.out.println("Null Value Exception");
+	}
+}
+}
